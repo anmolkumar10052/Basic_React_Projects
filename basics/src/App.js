62f53b1f781c0fd1ddd059bb89e67ebfd082dcdb
anmolkumar10052 <anmolkumar10052@gmail.com>
2024-12-26 12:51:03 +0530

    props concept

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,8 +1,21 @@
-
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+import Greeting from './Greeting';
 
 function App() {
+  
   return (
-    <h1>Hello</h1>
+    <div>
+      <h1>Hello from Reacjs</h1>
+      <Home name= {'Anmol'} age ={24} />
+      <Contact />
+      <Home />
+      <About />
+      <Greeting name = {'Anmol'} />
+      <Greeting name = {'Ayush'} />
+
+    </div>
   );
 }
 
